1477a6ef4396f68102d61da0b33be4136a7f28c2
LeontineC <79845918+LeontineC@users.noreply.github.com>
2022-02-21 07:42:03 +0100

    a tags

--- script.js
+++ script.js
@@ -36,11 +36,11 @@ setTimeout(startOpacity, 1000);
 
 // EVENT LISTENERS
 
-handlePointer = (e) => {
+/*handlePointer = (e) => {
   pointer.style.cssText =
     "left: " + e.clientX + "px; top: " + e.clientY + "px;";
   
-}; 
+}; */
 
 handleHamburger = (e) => {
   list.classList.toggle("active");
@@ -53,7 +53,7 @@ handleList = (e) => {
   list.classList.remove("active");
 };
 
-document.addEventListener("mousemove", handlePointer);
+// document.addEventListener("mousemove", handlePointer);
 hamburger.addEventListener("click", handleHamburger);
 listItems.forEach((item) => item.addEventListener("click", handleList));
 
