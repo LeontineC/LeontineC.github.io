d5c0ba23b81b36812abf9d387aa1759b8aa97f63
LeontineC <79845918+LeontineC@users.noreply.github.com>
2022-02-21 06:58:06 +0100

    work in progress

--- script.js
+++ script.js
@@ -1,110 +1,106 @@
-const animation = document.querySelector('.animation');
-const wrapper = document.querySelector('.wrapper');
-const pointer = document.querySelector('.pointer');
-const hamburger = document.querySelector('.hamburger');
-const list = document.querySelector('.navlist');
-const lines = document.querySelectorAll('.line');
-const listItems = document.querySelectorAll('li');
-const text = document.querySelectorAll('.list-text');
-const image = document.querySelectorAll('.image');
-const images = document.querySelector('.images');
-const projectImages = document.querySelectorAll('.project-img');
-const card = document.querySelectorAll('.card');
-
-
+const animation = document.querySelector(".animation");
+const wrapper = document.querySelector(".wrapper");
+const pointer = document.querySelector(".pointer");
+const hamburger = document.querySelector(".hamburger");
+const list = document.querySelector(".navlist");
+const lines = document.querySelectorAll(".line");
+const listItems = document.querySelectorAll("li");
+const text = document.querySelectorAll(".list-text");
+const image = document.querySelectorAll(".image");
+const images = document.querySelector(".images");
+const projectImages = document.querySelectorAll(".project-img");
+const card = document.querySelectorAll(".card");
 
 //ICONS
 feather.replace();
 
 //OPENING ANIMATION TIMEOUTS
 
-stopDisplay = () => {
-    animation.style.display = "none";
-}
+// stopDisplay = () => {
+  // animation.style.display = "none";
+// };
 
-stopOpacity = () => {
-    animation.style.opacity = 0;
-}
+// stopOpacity = () => {
+  // animation.style.opacity = 0;
+// };
 
 startOpacity = () => {
-    wrapper.style.opacity = 1;
-    document.body.style.overflow = "visible";
-    document.body.style.overflowX = "hidden";
-    
-}
+  wrapper.style.opacity = 1;
+  // document.body.style.overflow = "visible";
+  // document.body.style.overflowX = "hidden";
+};
 
-setTimeout(stopDisplay, 10000);
-setTimeout(stopOpacity, 3000);
-setTimeout(startOpacity, 10000);
+// setTimeout(stopDisplay, 10000);
+// setTimeout(stopOpacity, 3000);
+setTimeout(startOpacity, 1000);
 
 // EVENT LISTENERS
 
 handlePointer = (e) => {
-    pointer.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
+  pointer.style.cssText =
+    "left: " + e.clientX + "px; top: " + e.clientY + "px;";
 };
 
 handleHamburger = (e) => {
-   list.classList.toggle('active');
-   lines.forEach(line => {
-       line.classList.toggle('clicked');
-   })
+  list.classList.toggle("active");
+  lines.forEach((line) => {
+    line.classList.toggle("clicked");
+  });
 };
 
 handleList = (e) => {
-    list.classList.remove('active');
-} 
+  list.classList.remove("active");
+};
 
-document.addEventListener('mousemove', handlePointer);
-hamburger.addEventListener('click', handleHamburger);
-listItems.forEach(item => item.addEventListener('click', handleList));
+document.addEventListener("mousemove", handlePointer);
+hamburger.addEventListener("click", handleHamburger);
+listItems.forEach((item) => item.addEventListener("click", handleList));
 
 // INTERSECTION OBSERVERS
 
-
-
 changeImage = (entries) => {
-    entries.forEach(entry => {
-        // console.log(entry);
-        if(!entry.isIntersecting) {
-            images.lastElementChild.classList.remove('observing');
-            images.firstElementChild.classList.remove('action');
-            images.children[1].classList.remove('acting');
-          
-        } else {
-            images.lastElementChild.classList.add('observing');
-            images.firstElementChild.classList.add('action');
-            images.children[1].classList.add('acting');
-        }
-    })
+  entries.forEach((entry) => {
+    // console.log(entry);
+    if (!entry.isIntersecting) {
+      images.lastElementChild.classList.remove("observing");
+      images.firstElementChild.classList.remove("action");
+      images.children[1].classList.remove("acting");
+    } else {
+      images.lastElementChild.classList.add("observing");
+      images.firstElementChild.classList.add("action");
+      images.children[1].classList.add("acting");
+    }
+  });
 };
 
-/*loadImage = (entries) => {
-    entries.forEach(entry => {
-        console.log(entry);
-       if(entry.isIntersecting) {
-           entry.target.classList.add('loaded')
-       } else {
-           entry.target.classList.remove('loaded')
-       }
-    })
-    
-} */
+loadImage = (entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("loaded");
+    } else {
+      entry.target.classList.remove("loaded");
+    }
+  });
+};
 
 let options = {
-    root: null,
-    threshold: 1,
-    rootMargin: '0px',
-}
+  root: null,
+  threshold: 1,
+  rootMargin: "0px",
+};
 
-/*let projectOptions = {
-    root: null,
-    threshold: .5,
-    rootMargin: '0px',
-} */
+let projectOptions = {
+  root: null,
+  threshold: 0.5,
+  rootMargin: "0px",
+};
 
 const introObserver = new IntersectionObserver(changeImage, options);
-// const projectObserver = new IntersectionObserver(loadImage, projectOptions);
+const projectObserver = new IntersectionObserver(loadImage, projectOptions);
 
-introObserver.observe(images.lastElementChild); 
+introObserver.observe(images.lastElementChild);
 
-// card.forEach( picture => {projectObserver.observe(picture);});
+card.forEach((picture) => {
+  projectObserver.observe(picture);
+});
